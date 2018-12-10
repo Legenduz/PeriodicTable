bc1a5e4e91b862e4eda80c413cd3e876fb9bfdde
Legenduz <45770956+Legenduz@users.noreply.github.com>
2018-12-11 00:55:38 +0500

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,911 @@
+#include <iostream>
+#include <string>
+using namespace std;
+class element
+{
+	string name;
+	string symbol;
+	int atomicno;
+	float atomicmass;
+public:
+	void H()
+	{
+		atomicno = 1;
+		atomicmass = 1.0072;
+		name = "Hydrogen";
+		symbol = "H";
+	}
+	void He()
+	{
+		atomicno = 2;
+		atomicmass = 4.0026;
+		name = "Helium";
+		symbol = "He";
+	}
+	void Li()
+	{
+		atomicno = 3;
+		atomicmass = 6.941;
+		name = "Lithium";
+		symbol = "Li";
+	}
+	void Be()
+	{
+		atomicno = 4;
+		atomicmass = 9.0121;
+		name = "beryllium";
+		symbol = "Be";
+	}
+	void B()
+	{
+		atomicno = 5;
+		atomicmass = 10.811;
+		name = "Boron";
+		symbol = "B";
+	}
+	void C()
+	{
+		atomicno = 6;
+		atomicmass = 12.0107;
+		name = "Carbon";
+		symbol = "C";
+	}
+	void N()
+	{
+		atomicno = 7;
+		atomicmass = 14.0067;
+		name = "Nitrogen";
+		symbol = "N";
+	}
+	void O()
+	{
+		atomicno = 8;
+		atomicmass = 15.9994;
+		name = "Oxygen";
+		symbol = "O";
+	}
+	void F()
+	{
+		atomicno = 9;
+		atomicmass = 18.9984;
+		name = "Fluorine";
+		symbol = "F";
+	}
+	void Ne()
+	{
+		atomicno = 10;
+		atomicmass = 20.1797;
+		name = "Neon";
+		symbol = "Ne";
+	}
+	void Na()
+	{
+		atomicno = 11;
+		atomicmass = 22.9879;
+		name = "Sodium";
+		symbol = "Na";
+	}
+	void Mg()
+	{
+		atomicno = 12;
+		atomicmass = 24.3050;
+		name = "Magnesium";
+		symbol = "Mg";
+	}
+	void Al()
+	{
+		atomicno = 13;
+		atomicmass = 26.9815;
+		name = "Alluminium";
+		symbol = "Al";
+	}
+	void Si()
+	{
+		atomicno = 14;
+		atomicmass = 28.0855;
+		name = "Sillicon";
+		symbol = "Si";
+	}
+	void P()
+	{
+		atomicno = 15;
+		atomicmass = 30.9737;
+		name = "Phosphorus";
+		symbol = "P";
+	}
+	void S()
+	{
+		atomicno = 16;
+		atomicmass = 32.065;
+		name = "Sulphur";
+		symbol = "S";
+	}
+	void Cl()
+	{
+		atomicno = 17;
+		atomicmass = 35.453;
+		name = "Chlorine";
+		symbol = "Cl";
+	}
+	void Ar()
+	{
+		atomicno = 18;
+		atomicmass = 39.948;
+		name = "Argon";
+		symbol = "Ar";
+	}
+	void K()
+	{
+		atomicno = 19;
+		atomicmass = 39.0983;
+		name = "Potassium";
+		symbol = "K";
+	}
+	void Ca()
+	{
+		atomicno = 20;
+		atomicmass = 40.078;
+		name = "Calcium";
+		symbol = "Ca";
+	}
+	void Sc()
+	{
+		atomicno = 21;
+		atomicmass = 44.9559;
+		name = "Scandium";
+		symbol = "Sc";
+	}
+	void Ti()
+	{
+		atomicno = 22;
+		atomicmass = 47.867;
+		name = "Titanium";
+		symbol = "Ti";
+	}
+	void V()
+	{
+		atomicno = 23;
+		atomicmass = 50.9451;
+		name = "Vanadium";
+		symbol = "V";
+	}
+	void Cr()
+	{
+		atomicno = 24;
+		atomicmass = 51.9961;
+		name = "Chromium";
+		symbol = "Cr";
+	}
+	void Mn()
+	{
+		atomicno = 25;
+		atomicmass = 54.9380;
+		name = "Manganese";
+		symbol = "Mg";
+	}
+	void Fe()
+	{
+		atomicno = 26;
+		atomicmass = 55.845;
+		name = "Iron";
+		symbol = "Fe";
+	}
+	void Co()
+	{
+		atomicno = 27;
+		atomicmass = 58.9331;
+		name = "Cobalt";
+		symbol = "Co";
+	}
+	void Ni()
+	{
+		atomicno = 28;
+		atomicmass = 58.6934;
+		name = "Nickel";
+		symbol = "Ni";
+	}
+	void Cu()
+	{
+		atomicno = 29;
+		atomicmass = 63.546;
+		name = "Copper";
+		symbol = "Cu";
+	}
+	void Zn()
+	{
+		atomicno = 30;
+		atomicmass = 65.409;
+		name = "Zinc";
+		symbol = "Zn";
+	}
+	void Ga()
+	{
+		atomicno = 31;
+		atomicmass = 69.723;
+		name = "Gallium";
+		symbol = "Ga";
+	}
+	void Ge()
+	{
+		atomicno = 32;
+		atomicmass = 72.64;
+		name = "Germanium";
+		symbol = "Ge";
+	}
+	void As()
+	{
+		atomicno = 33;
+		atomicmass = 74.9216;
+		name = "Arsenic";
+		symbol = "As";
+	}
+	void Se()
+	{
+		atomicno = 34;
+		atomicmass = 78.96;
+		name = "Selinium";
+		symbol = "Se";
+	}
+	void Br()
+	{
+		atomicno = 35;
+		atomicmass = 79.904;
+		name = "Bromine";
+		symbol = "Br";
+	}
+	void Kr()
+	{
+		atomicno = 36;
+		atomicmass = 83.798;
+		name = "Krypton";
+		symbol = "Kr";
+	}
+	void Rb()
+	{
+		atomicno = 37;
+		atomicmass = 85.467;
+		name = "Rubidium";
+		symbol = "Rb";
+	}
+	void Sr()
+	{
+		atomicno = 38;
+		atomicmass = 87.62;
+		name = "Strontium";
+		symbol = "Sr";
+	}
+	void Y()
+	{
+		atomicno = 39;
+		atomicmass = 88.905;
+		name = "Yttrium";
+		symbol = "Y";
+	}
+	void Zr()
+	{
+		atomicno = 40;
+		atomicmass = 91.224;
+		name = "Zirconium";
+		symbol = "Zr";
+	}
+	void Nb()
+	{
+		atomicno = 41;
+		atomicmass = 92.906;
+		name = "Niobium";
+		symbol = "Nb";
+	}
+	void Mo()
+	{
+		atomicno = 42;
+		atomicmass = 95.94;
+		name = "Molybdenum";
+		symbol = "Mo";
+	}
+	void Tc()
+	{
+		atomicno = 43;
+		atomicmass = 98.906;
+		name = "Technetium";
+		symbol = "Tc";
+	}
+	void Ru()
+	{
+		atomicno = 44;
+		atomicmass = 101.07;
+		name = "Ruthenium";
+		symbol = "Ru";
+	}
+	void Rh()
+	{
+		atomicno = 45;
+		atomicmass = 102.905;
+		name = "Rhodium";
+		symbol = "Rh";
+	}
+	void Pd()
+	{
+		atomicno = 46;
+		atomicmass = 106.42;
+		name = "Plladium";
+		symbol = "Pd";
+	}
+	void Ag()
+	{
+		atomicno = 47;
+		atomicmass = 107.868;
+		name = "Silver";
+		symbol = "Ag";
+	}
+	void Cd()
+	{
+		atomicno = 48;
+		atomicmass = 112.411;
+		name = "Cadmium";
+		symbol = "Cd";
+	}
+	void In()
+	{
+		atomicno = 49;
+		atomicmass = 114.818;
+		name = "Indium";
+		symbol = "In";
+	}
+	void Sn()
+	{
+		atomicno = 50;
+		atomicmass = 118.710;
+		name = "Tin";
+		symbol = "Sn";
+	}
+	void Sb()
+	{
+		atomicno = 51;
+		atomicmass = 121.760;
+		name = "Antimony";
+		symbol = "Sb";
+	}
+	void Te()
+	{
+		atomicno = 52;
+		atomicmass = 127.60;
+		name = "Tellurium";
+		symbol = "Te";
+	}
+	void I()
+	{
+		atomicno = 53;
+		atomicmass = 126.904;
+		name = "Iodine";
+		symbol = "I";
+	}
+	void Xe()
+	{
+		atomicno = 54;
+		atomicmass = 131.293;
+		name = "Xenon";
+		symbol = "Xe";
+	}
+	void Cs()
+	{
+		atomicno = 55;
+		atomicmass = 132.905;
+		name = "Caesium";
+		symbol = "Cs";
+	}
+	void Ba()
+	{
+		atomicno = 56;
+		atomicmass = 137.327;
+		name = "barium";
+		symbol = "Ba";
+	}
+	void La()
+	{
+		atomicno = 57;
+		atomicmass = 138.905;
+		name = "Lanthanum";
+		symbol = "La";
+	}
+	void Ce()
+	{
+		atomicno = 58;
+		atomicmass = 140.116;
+		name = "Cerium";
+		symbol = "Ce";
+	}
+	void Pr()
+	{
+		atomicno = 59;
+		atomicmass = 140.904;
+		name = "Praseodymium";
+		symbol = "Pr";
+	}
+	void Nd()
+	{
+		atomicno = 60;
+		atomicmass = 144.242;
+		name = "Neodymium";
+		symbol = "Nd";
+	}
+	void Pm()
+	{
+		atomicno = 61;
+		atomicmass = 146.915;
+		name = "Promethium";
+		symbol = "146.915";
+	}
+	void Sm()
+	{
+		atomicno = 62;
+		atomicmass = 150.36;
+		name = "Samarium";
+		symbol = "Sm";
+	}
+	void Eu()
+	{
+		atomicno = 63;
+		atomicmass = 151.964;
+		name = "Europium";
+		symbol = "Eu";
+	}
+	void Gd()
+	{
+		atomicno = 64;
+		atomicmass = 157.25;
+		name = "Gadolinium";
+		symbol = "Gd";
+	}
+	void Tb()
+	{
+		atomicno = 65;
+		atomicmass = 158.925;
+		name = "Terbium";
+		symbol = "Tb";
+	}
+	void Dy()
+	{
+		atomicno = 66;
+		atomicmass = 162.500;
+		name = "Dysprosium";
+		symbol = "Dy";
+	}
+	void Ho()
+	{
+		atomicno = 67;
+		atomicmass = 164.93;
+		name = "Holomium";
+		symbol = "Ho";
+	}
+	void Er()
+	{
+		atomicno = 68;
+		atomicmass = 167.259;
+		name = "Erbium";
+		symbol = "Er";
+	}
+	void Tm()
+	{
+		atomicno = 69;
+		atomicmass = 168.934;
+		name = "Thulium";
+		symbol = "Tm";
+	}
+	void Yb()
+	{
+		atomicno = 70;
+		atomicmass = 173.04;
+		name = "Ytterbium";
+		symbol = "Yb";
+	}
+	void Lu()
+	{
+		atomicno = 71;
+		atomicmass = 174.967;
+		name = "Lutetium";
+		symbol = "Lu";
+	}
+	void Hf()
+	{
+		atomicno = 72;
+		atomicmass = 178.49;
+		name = "Hafnuim";
+		symbol = "Hf";
+	}
+	void Ta()
+	{
+		atomicno = 73;
+		atomicmass = 180.947;
+		name = "Tantalum";
+		symbol = "Ta";
+	}
+	void W()
+	{
+		atomicno = 74;
+		atomicmass = 183.84;
+		name = "Tungsten";
+		symbol = "W";
+	}
+	void Re()
+	{
+		atomicno = 75;
+		atomicmass = 186.207;
+		name = "Rhenium";
+		symbol = "Re";
+	}
+	void Os()
+	{
+		atomicno = 76;
+		atomicmass = 190.23;
+		name = "Osmium";
+		symbol = "Os";
+	}
+	void Ir()
+	{
+		atomicno = 77;
+		atomicmass = 192.217;
+		name = "Iridium";
+		symbol = "Ir";
+	}
+	void Pt()
+	{
+		atomicno = 78;
+		atomicmass = 195.084;
+		name = "Platinum";
+		symbol = "Pt";
+	}
+	void Au()
+	{
+		atomicno = 79;
+		atomicmass = 196.966;
+		name = "Gold";
+		symbol = "Au";
+	}
+	void Hg()
+	{
+		atomicno = 80;
+		atomicmass = 200.59;
+		name = "Mercury";
+		symbol = "Hg";
+	}
+	void Tl()
+	{
+		atomicno = 81;
+		atomicmass = 204.383;
+		name = "Thallium";
+		symbol = "Ti";
+	}
+	void Pb()
+	{
+		atomicno = 82;
+		atomicmass = 207.2;
+		name = "Plumbum";
+		symbol = "Pb";
+	}
+	void Bi()
+	{
+		atomicno = 83;
+		atomicmass = 208.9804;
+		name = "Bismuth";
+		symbol = "Bi";
+	}
+	void Po()
+	{
+		atomicno = 84;
+		atomicmass = 208.9824;
+		name = "Polonium";
+		symbol = "Po";
+	}
+	void At()
+	{
+		atomicno = 85;
+		atomicmass = 209.9871;
+		name = "Astatine";
+		symbol = "At";
+	}
+	void Rn()
+	{
+		atomicno = 86;
+		atomicmass = 222.0176;
+		name = "Radon";
+		symbol = "Rn";
+	}
+	void Fr()
+	{
+		atomicno = 87;
+		atomicmass = 223.0197;
+		name = "Francium";
+		symbol = "Fr";
+	}
+	void Ra()
+	{
+		atomicno = 88;
+		atomicmass = 226.0254;
+		name = "Radium";
+		symbol = "Ra";
+	}
+	void Ac()
+	{
+		atomicno = 89;
+		atomicmass = 227.0278;
+		name = "Actinium";
+		symbol = "Ac";
+	}
+	void Th()
+	{
+		atomicno = 90;
+		atomicmass = 232.0380;
+		name = "Thorium";
+		symbol = "Th";
+	}
+	void Pa()
+	{
+		atomicno = 91;
+		atomicmass = 231.0358;
+		name = "Protactinium";
+		symbol = "Pa";
+	}
+	void U()
+	{
+		atomicno = 92;
+		atomicmass = 238.0289;
+		name = "Uranium";
+		symbol = "U";
+	}
+	void Np()
+	{
+		atomicno = 93;
+		atomicmass = 237.0482;
+		name = "Neptunium";
+		symbol = "Np";
+	}
+	void Pu()
+	{
+		atomicno = 94;
+		atomicmass = 244.0642;
+		name = "Plutonium";
+		symbol = "Pu";
+	}
+	void Am()
+	{
+		atomicno = 95;
+		atomicmass = 243.0614;
+		name = "Americium";
+		symbol = "Am";
+	}
+	void Cm()
+	{
+		atomicno = 96;
+		atomicmass = 247.0703;
+		name = "Curium";
+		symbol = "Cm";
+	}
+	void Bk()
+	{
+		atomicno = 97;
+		atomicmass = 247.0703;
+		name = "Berkelium";
+		symbol = "Bk";
+	}
+	void Cf()
+	{
+		atomicno = 98;
+		atomicmass = 251.0796;
+		name = "Californium";
+		symbol = "Cf";
+	}
+	void Es()
+	{
+		atomicno = 99;
+		atomicmass = 252.0829;
+		name = "Einsteinium";
+		symbol = "Es";
+	}
+	void Fm()
+	{
+		atomicno = 100;
+		atomicmass = 257.0951;
+		name = "Fermium";
+		symbol = "Fm";
+	}
+	void Md()
+	{
+		atomicno = 101;
+		atomicmass = 258.0951;
+		name = "Mendelevium";
+		symbol = "Md";
+	}
+	void No()
+	{
+		atomicno = 102;
+		atomicmass = 259.1009;
+		name = "Nobelium";
+		symbol = "No";
+	}
+	void Lr()
+	{
+		atomicno = 103;
+		atomicmass = 266.1193;
+		name = "Lawrencium";
+		symbol = "Lr";
+	}
+	void Rf()
+	{
+		atomicno = 104;
+		atomicmass = 261;
+		name = "Rutherfordium";
+		symbol = "Rf";
+	}
+	void Db()
+	{
+		atomicno = 105;
+		atomicmass = 262;
+		name = "Dubinium";
+		symbol = "Db";
+	}
+	void Sg()
+	{
+		atomicno = 106;
+		atomicmass = 262;
+		name = "Seaborgium";
+		symbol = "Sg";
+	}
+	void Bh()
+	{
+		atomicno = 107;
+		atomicmass = 264;
+		name = "Bohrium";
+		symbol = "Bh";
+	}
+	void Hs()
+	{
+		atomicno = 108;
+		atomicmass = 269;
+		name = "Hassium";
+		symbol = "Hs";
+	}
+	void Mt()
+	{
+		atomicno = 109;
+		atomicmass = 268;
+		name = "Meitnerium";
+		symbol = "Mt";
+	}
+	void Ds()
+	{
+		atomicno = 110;
+		atomicmass = 281.1620;
+		name = "Darmstadium";
+		symbol = "Ds";
+	}
+	void Rg()
+	{
+		atomicno = 111;
+		atomicmass = 281.1684;
+		name = "Roentgenium";
+		symbol = "Rg";
+	}
+	void Cn()
+	{
+		atomicno = 112;
+		atomicmass = 285.1744;
+		name = "Copernicium";
+		symbol = "Cn";
+	}
+	void output()
+	{
+		cout << "Name:" << name << endl;
+		cout << "atomic number:" << atomicno << endl;
+		cout << "atomic mass:" << atomicmass << endl;
+		cout << "symbol:" << symbol << endl;
+		cout << "*******************************************************************" << endl;
+
+	}
+	float getatomicmass()
+	{
+		return atomicmass;
+	}
+	int getatomicno()
+	{
+		return atomicno;
+	}
+};
+void menu()
+{
+	cout << "Press 1 to see elements startin g from H." << endl;
+	cout << "Press 2 to see the elements having Atomic mass betwee 1 and 50." << endl;
+	cout << "Press 3 to view te transition elements." << endl;
+	cout << "Press 4 to view metals and non metals" << endl;
+	cout << "Press 5 to exit" << endl;
+}
+void main()
+{
+	int opt;
+	element e[112];
+	e[0].H();	e[1].He();	e[2].Li();	e[3].Be();	e[4].B();		e[5].C();		e[6].N();		e[7].O();		e[8].F();
+	e[9].Ne();	e[10].Na();	e[11].Mg();	e[12].Al();	e[13].Si();	e[14].P();	e[15].S();	e[16].Cl();	e[17].Ar();	e[18].K();	e[19].Ca();	e[20].Sc();
+	e[21].Ti();	e[22].V();	e[23].Cr();	e[24].Mn();	e[25].Fe();	e[26].Co();	e[27].Ni();	e[28].Cu();	e[29].Zn();	e[30].Ga();
+	e[31].Ge();	e[32].As();	e[33].Se();	e[34].Br();	e[35].Kr();	e[36].Rb();	e[37].Sr();	e[38].Y();	e[39].Zr();	e[40].Nb();
+	e[41].Mo();	e[42].Tc();	e[43].Ru();	e[44].Rh();	e[45].Pd();	e[46].Ag();	e[47].Cd();	e[48].In();	e[49].Sn();	e[50].Sb();
+	e[51].Te();	e[52].I();	e[53].Xe();	e[54].Cs();	e[55].Ba();	e[56].La();	e[57].Ce();	e[58].Pr();	e[59].Nd();	e[60].Pm();
+	e[61].Sm();	e[62].Eu();	e[63].Gd();	e[64].Tb();	e[65].Dy();	e[66].Ho();	e[67].Er();	e[68].Tm();	e[69].Yb();	e[70].Lu();
+	e[71].Hf();	e[72].Ta();	e[73].W();	e[74].Re();	e[75].Os();	e[76].Ir();	e[77].Pt();	e[78].Au();	e[79].Hg();	e[80].Tl();
+	e[81].Pb();	e[82].Bi();	e[83].Po();	e[84].At();	e[85].Rn();	e[86].Fr();	e[87].Ra();	e[88].Ac();	e[89].Th();	e[90].Pa();
+	e[91].U();	e[92].Np();	e[93].Pu();	e[94].Am();	e[95].Cm();	e[96].Bk();	e[97].Cf();	e[98].Es();	e[99].Fm();	e[100].Md();
+	e[101].No();	e[102].Lr();	e[103].Rf();	e[104].Db();	e[105].Sg();	e[106].Bh();	e[107].Hs();	e[108].Mt();	e[109].Ds();	e[110].Rg();
+	e[111].Cn();
+	do
+	{
+		menu();
+		cin >> opt;
+		if (opt == 1)
+		{
+			system("CLS");
+			e[0].output();
+			e[1].output();
+			e[71].output();
+			e[107].output();
+			e[66].output();
+		}
+		if (opt == 2)
+		{
+			system("CLS");
+			for (int i = 0; i < 112; i++)
+			{
+				if (e[i].getatomicmass() >= 10 && e[i].getatomicmass() <= 50)
+					e[i].output();
+			}
+		}
+		if (opt == 3)
+		{
+			for (int i = 0;i <= 112;i++)
+			{
+				if (e[i].getatomicno() >= 21 && e[i].getatomicno() <= 30)
+					e[i].output();
+				if (e[i].getatomicno() >= 39 && e[i].getatomicno() <= 48)
+					e[i].output();
+				if (e[i].getatomicno() >= 57 && e[i].getatomicno() <= 80)
+					e[i].output();
+				if (e[i].getatomicno() >= 89 && e[i].getatomicno() <= 112)
+					e[i].output();
+			}
+		}
+		if (opt == 4)
+		{
+			cout << "Metals:" << endl << "***************************************************************************************************************************************************************************************" << endl;
+			for (int i = 0;i < 112;i++)
+			{
+				if (e[i].getatomicno() == 3 && e[i].getatomicno() == 4)
+					e[i].output();
+				if (e[i].getatomicno() >= 11 && e[i].getatomicno() <= 13)
+					e[i].output();
+				if (e[i].getatomicno() >= 19 && e[i].getatomicno() <= 31)
+					e[i].output();
+				if (e[i].getatomicno() >= 37 && e[i].getatomicno() <= 50)
+					e[i].output();
+				if (e[i].getatomicno() >= 55 && e[i].getatomicno() <= 84)
+					e[i].output();
+				if (e[i].getatomicno() >= 87 && e[i].getatomicno() <= 112)
+					e[i].output();
+			}
+			cout << "Non-Metals:" << endl << "***************************************************************************************************************************************************************************************" << endl;
+			for (int i = 0; i < 112; i++)
+			{
+				if (e[i].getatomicno() == 1 && e[i].getatomicno()==2)
+					e[i].output();
+				if (e[i].getatomicno() >= 6 && e[i].getatomicno() <= 10)
+					e[i].output();
+				if (e[i].getatomicno() >= 15 && e[i].getatomicno() <= 18)
+					e[i].output();
+				if (e[i].getatomicno() >= 34 && e[i].getatomicno() <= 36)
+					e[i].output();
+				if (e[i].getatomicno() >= 53 && e[i].getatomicno() <= 55)
+					e[i].output();
+				if (e[i].getatomicno() == 117 && e[i].getatomicno()== 118)
+					e[i].output();
+			}
+
+		}
+
+	} while (opt!=5);
+}
